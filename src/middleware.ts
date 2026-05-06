56e0e01103902356696aa2e450dbbc92a6de3eb3
codetokoni <30356989+codetokoni@users.noreply.github.com>
2026-05-06 22:39:41 +0100

    feat(middleware): allow public access to kingschat oauth routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-up(.*)",
   "/room/(.*)",
   "/api/livekit/token(.*)",
+  "/api/auth/kingschat/(.*)",
 ]);
 
 export default clerkMiddleware(
@@ -16,7 +17,7 @@ export default clerkMiddleware(
   },
   {
     authorizedParties: [
-            "https://neoconference.vercel.app",
+      "https://neoconference.vercel.app",
       "https://special-space-potato-5v6vj4v99r4h474p-3000.app.github.dev",
       "http://localhost:3000",
     ],
